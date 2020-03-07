6db2983efe2560ec5b547274178cdf1b3f8f2be0
denvolk91 <denvolk91@gmail.com>
2020-03-07 11:33:36 +0300

    3.3. Разбор первой программы.[#250701]

--- src/ru/job4j/StudentInfo.java
+++ src/ru/job4j/StudentInfo.java
@@ -1,4 +1,8 @@
 package ru.job4j;
 
 public class StudentInfo {
+    public static void main(String[] args){
+        System.out.println("Denis Volk");
+        System.out.println("04.04.1991");
+    }
 }
