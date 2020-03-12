5a7f2317ed451aeccbe7d80822750b209b8a5cf3
denvolk91 <denvolk91@gmail.com>
2020-03-12 17:58:06 +0300

    4.3.1. А существует ли треугольник?[#250651]

---
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -0,0 +1,9 @@
+package main.java.ru.job4j.condition;
+
+public class MultiMax {
+    public int max(int first, int second, int third) {
+        int result = first > second ? first : second;
+        result = result > third ? result : third;
+        return result;
+    }
+}
