159081cdccf174657e9752e05eacc662e171ec84
denvolk91 <denvolk91@gmail.com>
2020-03-12 17:39:06 +0300

    4.3.1. А существует ли треугольник?[#250651]

---
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -0,0 +1,21 @@
+package test.java.ru.job4j.condition;
+
+import main.java.ru.job4j.condition.Triangle;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class TriangleTest {
+
+    @Test
+    public void whenExistTrue() {
+        boolean result = Triangle.exist(2.0, 2.0, 2.0);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenExistFalse(){
+        boolean result = Triangle.exist(2.0, 2.0, 5.0);
+        assertThat(result, is(false));
+    }
+}
