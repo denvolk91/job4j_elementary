0c20fb8b7fa43d0c95e9bd21dc707032d24fcb6e
denvolk91 <denvolk91@gmail.com>
2020-03-13 18:28:48 +0300

    4.2. Тернарное сравнение[#250648]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -1,13 +1,13 @@
 package ru.job4j.condition;
 
 public class SqArea {
-    public static double square(int p, int k) {
-        int h = p / (2 * (k+1));
-        int l = h * k;
-        int s = l * h;
 
-        return s;
-    }
+        public static int square(int p, int k) {
+            int h = p / (2 * (k + 1));
+            int w = k * h;
+            int s = w + h;
+            return s;
+        }
     public static void main (String[] args) {
         double result1 = SqArea.square(6,2);
         System.out.println(" p = 6, k = 2, s = 2, real = " + result1);
