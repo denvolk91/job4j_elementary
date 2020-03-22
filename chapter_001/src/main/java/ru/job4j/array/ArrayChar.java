981784ffa599337d9fc5e734e1746f8650e10c01
Volk <denvolk91@gmail.com>
2020-03-23 00:55:00 +0300

    6.4. Слово начинается с ...[#250658]

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -6,8 +6,8 @@ public class ArrayChar {
         for (int i = 0; i < pref.length; i++) {
             if (pref[i] != word[i]) {
                 result = false;
-                }
             }
+        }
         return result;
     }
 }
