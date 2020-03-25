70b3f60dbc975d92684208884a5d2336fad4e409
Volk <denvolk91@gmail.com>
2020-03-25 23:39:21 +0300

    6.6.2. Двухмерный массив. Таблица умножения.[#250657]

--- chapter_001/src/main/java/ru/job4j/array/BarleyBreak.java
+++ chapter_001/src/main/java/ru/job4j/array/BarleyBreak.java
@@ -3,8 +3,8 @@ package main.java.ru.job4j.array;
 public class BarleyBreak {
     public static void main(String[] args) {
         int[][] table = new int[3][3];
-        for (int i = 0; i < 3; ++i) {
-            for (int j = 0; j < 3; ++j) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 table[i][j] = (i * 3) + j + 1;
                 System.out.print(table[i][j] + " ");
             }
---
+++ chapter_001/src/test/java/ru/job4j/array/MatrixTest.java
@@ -0,0 +1,19 @@
+package test.java.ru.job4j.array;
+
+import main.java.ru.job4j.array.Matrix;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MatrixTest {
+    @Test
+    public void when2on2() {
+        Matrix matrix = new Matrix();
+        int[][] table = matrix.multiple(2);
+        int[][] expect = {
+                {1, 2},
+                {2, 4}
+        };
+        assertThat(table, is(expect));
+    }
+}
