a806da678645cf7ce97a579da3ecaff4302852b3
Volk <denvolk91@gmail.com>
2020-03-25 18:06:16 +0300

    6.5. Поиск индекса в диапазоне.[#250663]

---
+++ chapter_001/src/main/java/ru/job4j/array/SortSelected.java
@@ -0,0 +1,10 @@
+package main.java.ru.job4j.array;
+
+public class SortSelected {
+
+    public static int[] sort(int[] data) {
+        int min = MinDiapason.findMin(data, 0, data.length);
+        int index = FindLoop.indexOf(data, min, 0, data.length);
+        return data;
+    }
+}
