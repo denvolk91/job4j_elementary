b8d8d68d5c854ac2dd42413618f9fe574b17333d
Volk <denvolk91@gmail.com>
2020-03-26 19:34:50 +0300

    6.7.2. Моно столбец в матрице.[#250668]

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -16,14 +16,12 @@ public class MatrixCheck {
     public static boolean monoVertical(char[][] board, int column) {
         boolean result = true;
         for (int i = 0; i < board.length; i++) {
-            for (int j = column; j < board.length; j++) {
-                board[i][j] = 'X';
-                if (board[i][j] != 'X') {
+                board[i][column] = 'X';
+                if (board[i][column] != 'X') {
                     result = false;
                     break;
                 }
             }
-        }
         return result;
     }
     public static char[] extractDiagonal(char[][] board) {
