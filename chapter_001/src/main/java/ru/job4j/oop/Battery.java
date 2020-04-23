ed63838bc47adcd16db31a9c3c6dc73c1795c8e9
Volk <denvolk91@gmail.com>
2020-04-23 13:56:27 +0300

    1.7. Состояние объекта.[#250831]

--- chapter_001/src/main/java/ru/job4j/oop/Battery.java
+++ chapter_001/src/main/java/ru/job4j/oop/Battery.java
@@ -8,8 +8,8 @@ public class Battery {
     }
 
     public void exchange(Battery another) {
-        this.load = this.load + another.load;
-        another.load = 0;
+        another.load = another.load + this.load;
+        this.load = 0;
     }
 
     public static void main(String[] args) {
@@ -17,7 +17,7 @@ public class Battery {
         Battery powerbank = new Battery(50);
         System.out.println("До зарядки в телефоне: " + mobile.load
                 + " до зарядки телефона в пауербанке: " + powerbank.load);
-        mobile.exchange(powerbank);
+        powerbank.exchange(mobile);
         System.out.println("После зарядки в телефоне: " + mobile.load
                 + " после зарядки телефона в пауербанке: " + powerbank.load);
     }
