f36ef0a51cec632fdafb66e2d855b09f2ca3adb6
denvolk91 <39922440+denvolk91@users.noreply.github.com>
2020-04-29 00:00:01 +0300

    Перегрузка конструктора. [#250837]

--- chapter_001/src/main/java/ru/job4j/oop/Predator.java
+++ chapter_001/src/main/java/ru/job4j/oop/Predator.java
@@ -1,6 +1,12 @@
 package main.java.ru.job4j.oop;
 
 public class Predator extends Animal {
+
+    public Predator() {
+        super();
+        System.out.println("Predator");
+    }
+
     public Predator(String name) {
         super(name);
     }
--- chapter_001/src/main/java/ru/job4j/oop/Tiger.java
+++ chapter_001/src/main/java/ru/job4j/oop/Tiger.java
@@ -1,6 +1,11 @@
 package main.java.ru.job4j.oop;
 
 public class Tiger extends Predator {
+
+    public Tiger() {
+        super();
+        System.out.println("Tiger");
+    }
     public Tiger(String name) {
         super(name);
     }
