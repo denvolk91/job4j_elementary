27e99cfb6111f9b37293bf6ef7c1740a883eade0
denvolk91 <39922440+denvolk91@users.noreply.github.com>
2020-05-22 00:16:36 +0300

    Реализовать класс Tracker [#250849]

--- chapter_001/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_001/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,19 +1,15 @@
 package main.java.ru.job4j.tracker;
-
 import java.util.Arrays;
 import java.util.Random;
-
 public class Tracker {
     /**
      * Массив для хранения заявок.
      */
     private final Item[] items = new Item[100];
-
     /**
      * Указатель ячейки для новой заявки.
      */
     private int position = 0;
-
     /**
      * Метод добавления заявки в хранилище
      * @param item новая заявка
@@ -23,7 +19,6 @@ public class Tracker {
         items[position++] = item;
         return item;
     }
-
     /**
      * Метод генерирует уникальный ключ для заявки.
      * Так как у заявки нет уникальности полей, имени и описание. Для идентификации нам нужен уникальный ключ.
@@ -33,22 +28,13 @@ public class Tracker {
         Random rm = new Random();
         return String.valueOf(rm.nextLong() + System.currentTimeMillis());
     }
-
     /**
      * Метод возвращает копию массива items без null эллеиентов.
      * @return itemsWithoutNull массив без null
      */
+
     public Item[] findAll() {
-        Item[] itemsWithoutNull = new Item[items.length];
-        for (int i = 0; i < items.length; i++) {
-            Item itemBuf = items[i];
-            if (itemBuf != null) {
-                itemsWithoutNull[position] = itemBuf;
-                position++;
-            }
-            itemsWithoutNull = Arrays.copyOf(itemsWithoutNull, position);
-        }
-        return itemsWithoutNull;
+        return Arrays.copyOf(items, position);
     }
 
     /**
@@ -58,34 +44,32 @@ public class Tracker {
      * @return массив совпавших имён.
      */
     public Item[] findByName(String key) {
-        Item[] itemsWithoutEquals = new Item[items.length];
-
-        for (int i = 0; i < items.length; i++) {
+        Item[] itemsWithoutEquals = new Item[position];
+        int size = 0;
+        for (int i = 0; i < position; i++) {
             if (items[i].getName().equals(key)) {
-                itemsWithoutEquals[position] = items[i];
+                    itemsWithoutEquals[size] = items[i];
+                    size++;
+            }
+        }
+        return Arrays.copyOf(itemsWithoutEquals, size);
+    }
+
+        /**
+         * Метод проверяет в цикле все эллементы массива items, сравнивая id с аргументом String id
+         * и возвращает найденный Item. Если не найдет, то null.
+         * @param id получение заявки.
+         * @return найденное совпадение или null.
+         */
+        public Item findById(String id) {
+            Item item = null;
+            for (int i = 0; i < position; i++) {
+                if (items[i].getId().equals(id)) {
+                    item = items[i];
+                    break;
+                }
                 position++;
             }
-            itemsWithoutEquals = Arrays.copyOf(itemsWithoutEquals, position);
+            return item;
         }
-        return itemsWithoutEquals;
     }
-
-    /**
-     * Метод проверяет в цикле все эллементы массива items, сравнивая id с аргументом String id
-     * и возвращает найденный Item. Если не найдет, то null.
-     * @param id получение заявки.
-     * @return найденное совпадение или null.
-     */
-    public Item findById(String id) {
-        Item[] idByItems = new Item[items.length];
-        for (int i = 0; i < items.length; i++) {
-            if (items[i].getId().equals(id)) {
-                idByItems[position] = items[i];
-            } else {
-                idByItems[position] = null;
-            }
-            position++;
-        }
-        return idByItems[position];
-    }
-}
