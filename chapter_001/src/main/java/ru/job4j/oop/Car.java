5053cfa4965746950625711f40d4804f56a44f7e
denvolk91 <39922440+denvolk91@users.noreply.github.com>
2020-05-30 22:10:21 +0300

    Приведение типов. Повышение типа и понижение типа. [#250790]

---
+++ chapter_001/src/main/java/ru/job4j/oop/Car.java
@@ -0,0 +1,5 @@
+package main.java.ru.job4j.oop;
+
+public class Car extends Transport {
+
+}
---
+++ chapter_001/src/main/java/ru/job4j/univer/College.java
@@ -0,0 +1,9 @@
+package main.java.ru.job4j.univer;
+
+public class College {
+    public static void main(String[] args) {
+        Freshman freshman = new Freshman();
+        Student st = freshman;
+        Object obj = freshman;
+    }
+}
